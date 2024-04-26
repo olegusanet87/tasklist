f5fc87e2698b7934e7651fc88e1bcec481181c99
olegusanet87 <olegusanet@gmail.com>
2024-04-26 23:02:37 +0300

    create TaskList

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,19 @@
+import css from './Form.module.css';
+
+export default function Form({ onAdd }) {
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		onAdd({
+			id: Date.now(),
+			text: e.target.elements.text.value,
+		});
+		e.target.reset();
+	};
+
+	return (
+		<form className={css.form} onSubmit={handleSubmit}>
+			<input className={css.field} type="text" name="text" />
+			<button type="submit">Add task</button>
+		</form>
+	);
+}
